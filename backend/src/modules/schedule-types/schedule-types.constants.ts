ac2b704896a484c67df2fcad0e88cb79a7c568e0
Rodri <rriffo.99@gmail.com>
2026-02-01 12:06:00 +0000

    feat(backend): add schedule types constants

---
+++ backend/src/modules/schedule-types/schedule-types.constants.ts
@@ -0,0 +1,6 @@
+export const SCHEDULE_TYPE_PERMISSIONS = {
+  READ: 'schedule_types:read',
+  CREATE: 'schedule_types:create',
+  UPDATE: 'schedule_types:update',
+  DELETE: 'schedule_types:delete',
+} as const;
